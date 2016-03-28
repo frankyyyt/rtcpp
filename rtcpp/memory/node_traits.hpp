75e5546bc4b2206f221011ca260815fbe4ac4cea
Marcelo <mzimbres@gmail.com>
2016-03-28 08:36:04 -0300

    Code formating.

--- rtcpp/memory/node_traits.hpp
+++ rtcpp/memory/node_traits.hpp
@@ -26,18 +26,26 @@ RTCPP_HAS_NESTED_TYPE(value_type)
 
 template <typename T>
 struct is_node_type {
-  static const bool value = has_value_type<T>::value && has_pointer<T>::value;
+  static const bool value = has_value_type<T>::value &&
+    has_pointer<T>::value;
 };
 
 // Does not require node pointer to be the same.
-template <typename N1, typename N2, bool IsNode = is_node_type<N2>::value && is_node_type<N1>::value>
+template < typename N1
+         , typename N2
+         , bool IsNode = is_node_type<N2>::value &&
+           is_node_type<N1>::value>
 struct is_same_node_type {
-  static const bool has_same_value_type = std::is_same< typename N1::value_type
-                                                      , typename N2::value_type>::value;
+  static const bool has_same_value_type =
+    std::is_same< typename N1::value_type
+                , typename N2::value_type>::value;
   using value_type = typename N1::value_type;
-  using same_node_type1 = typename N1::template rebind<value_type, void*>::other;
-  using same_node_type2 = typename N2::template rebind<value_type, void*>::other;
-  static const bool value = has_same_value_type && std::is_same<same_node_type1, same_node_type2>::value;
+  using same_node_type1 = typename N1::template
+    rebind<value_type, void*>::other;
+  using same_node_type2 = typename N2::template
+    rebind<value_type, void*>::other;
+  static const bool value = has_same_value_type &&
+    std::is_same<same_node_type1, same_node_type2>::value;
 };
 
 template <typename N1, typename N2>
