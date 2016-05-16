a5242cb0db001dc2d9ca317168edffdb31b1f9b9
Marcelo <mzimbres@gmail.com>
2016-05-16 07:58:56 -0300

    Improvements in insertion sort.

--- rtcpp/algorithm/insertion_sort.hpp
+++ rtcpp/algorithm/insertion_sort.hpp
@@ -5,20 +5,27 @@
 namespace rt
 {
 
+template <class Iter, class Comp>
+void insertion_sort(Iter begin, Iter end, Comp comp)
+{
+  const auto rend = std::prev(begin);
+  for (Iter iter = ++begin; iter != end; ++iter) {
+    Iter prev = std::prev(iter, 1);
+    const auto K = *iter;
+    while (comp(K, *prev)) {
+      *std::next(prev, 1) = *prev;
+      if (--prev == rend)
+        break;
+    }
+    *std::next(prev, 1) = K;
+  }
+}
+
 template <class Iter>
 void insertion_sort(Iter begin, Iter end)
 {
-  const auto N = std::distance(begin, end);
-  for (int j = 1; j < N; ++j) {
-    int i = j - 1;
-    const auto K = begin[j];
-    while (!(K >= begin[i])) {
-      begin[i + 1] = begin[i];
-      if (--i == -1)
-        break;
-    }
-    begin[i + 1] = K;
-  }
+  using value_type = typename std::iterator_traits<Iter>::value_type;
+  insertion_sort(begin, end, std::less<value_type>());
 }
 
 }
--- src/tests/test_insertion_sort.cpp
+++ src/tests/test_insertion_sort.cpp
@@ -1,5 +1,7 @@
+#include <list>
 #include <vector>
 #include <limits>
+#include <iterator>
 #include <algorithm>
 
 #include <rtcpp/utility/make_rand_data.hpp>
@@ -7,11 +9,19 @@
 
 int main()
 {
-  std::vector<int> data =
-    rt::make_rand_data<int>(20000, 1, std::numeric_limits<int>::max());
+  const int N = 10000;
+  std::vector<int> data1 =
+    rt::make_rand_data<int>(N, 1, std::numeric_limits<int>::max());
 
-  rt::insertion_sort(std::begin(data), std::end(data));
+  std::list<int> data2;
+  std::copy(std::begin(data1), std::end(data1),
+            std::back_inserter(data2));
 
-  return std::is_sorted(std::begin(data), std::end(data)) ? 0 : 1;
+  rt::insertion_sort(std::begin(data1), std::end(data1));
+  rt::insertion_sort(std::begin(data2), std::end(data2));
+
+  const bool r1 = std::is_sorted(std::begin(data1), std::end(data1));
+  const bool r2 = std::is_sorted(std::begin(data2), std::end(data2));
+  return r1 && r2 ? 0 : 1;
 }
 
