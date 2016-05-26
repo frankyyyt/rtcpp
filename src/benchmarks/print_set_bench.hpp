0d42ce6b2b2eb7835233c883bbc590b9cf5cd632
Marcelo <mzimbres@gmail.com>
2016-05-26 16:52:58 -0300

    Small change in benchmark function to introduce traversal.

--- src/benchmarks/print_set_bench.hpp
+++ src/benchmarks/print_set_bench.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #include <rtcpp/utility/timer.hpp>
 
 namespace rt {
@@ -11,16 +13,22 @@ void print_set_bench(C c, Iter begin, std::size_t n)
   // and deletions are made toguether to maximize cache misses.
 
   const std::size_t s = n / 2;
+  int sum = 0;
+  int repeat = 30;
   c.insert(begin, begin + s); // Inserts the first half of the data.
   {
     rt::timer t;
     for (std::size_t i = 0; i <= s; ++i) {
       c.erase(begin[i]); // Removes the first half.
       c.insert(begin[n - i - 1]); // Inserts the second half.
+      if (i % repeat == 0)
+        sum = std::accumulate(std::begin(c), std::end(c), 0);
     }
     for (std::size_t i = 0; i <= s; ++i) {
       c.erase(begin[n - i - 1]); // Removes the second half.
       c.insert(begin[i]); // Inserts the first half again.
+      if (i % repeat == 0)
+        sum = std::accumulate(std::begin(c), std::end(c), 0);
     }
   }
 }
