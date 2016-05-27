9de102115f690760ca2a35573e02acbaeb25f769
Marcelo <mzimbres@gmail.com>
2016-05-27 07:35:56 -0300

    Improved docs and one ore benchmark graph.

--- src/benchmarks/print_set_bench.hpp
+++ src/benchmarks/print_set_bench.hpp
@@ -6,24 +6,32 @@
 
 namespace rt {
 
-template <typename C, typename Iter>
-void print_set_bench(C c, Iter begin, std::size_t n)
+template <typename C, typename RandomAccessIter>
+void print_set_bench(C c, RandomAccessIter begin, std::size_t n)
 {
-  // This function is designed to benchmark an std::set.  Insertions
+  // c: Set instance, can be an std::set, std::unordered_set, etc.
+  // begin: Iterator pointing to random data.
+  // n: Size of random data.
+  //
+  // This function is designed to benchmark an sets.  Insertions
   // and deletions are made toguether to maximize cache misses.
 
   const std::size_t s = n / 2;
-  int sum = 0;
+  int sum = 0; // Dummy variable.
   int repeat = 10;
-  c.insert(begin, begin + s); // Inserts the first half of the data.
+  // Inserts the first half of the random data in the set. do not
+  // participate in the benchmark.
+  c.insert(begin, begin + s);
   {
     rt::timer t;
     for (std::size_t i = 0; i <= s; ++i) {
-      c.erase(begin[i]); // Removes the first half.
-      c.insert(begin[n - i - 1]); // Inserts the second half.
+      c.erase(begin[i]); // Removes already inserted random data.
+      c.insert(begin[n - i - 1]); // Inserts the second half of rando data.
+      // Traverses the container every *repeat* times
       if (i % repeat == 0)
         sum = std::accumulate(std::begin(c), std::end(c), 0);
     }
+    // Same purpose as the loop above.
     for (std::size_t i = 0; i <= s; ++i) {
       c.erase(begin[n - i - 1]); // Removes the second half.
       c.insert(begin[i]); // Inserts the first half again.
