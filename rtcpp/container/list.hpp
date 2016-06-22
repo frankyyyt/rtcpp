ad4d4abc513f37ab0ef9cebd11dd0c96f4962732
Marcelo <mzimbres@gmail.com>
2016-06-22 15:37:58 -0300

    Addition of many functions to rt::list.

--- rtcpp/container/list.hpp
+++ rtcpp/container/list.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <utility>
 
 namespace rt
 {
@@ -9,9 +10,9 @@ template <class T>
 struct list_node {
   using pointer = list_node*;
   pointer next;
+  pointer prev;
   T info;
-  list_node() : next(nullptr), info() {}
-  list_node(pointer p, const T& v) : next(p), info(v) {}
+  list_node() : next(nullptr), prev(nullptr), info() {}
 };
 
 template <class T, class Allocator = std::allocator<T>>
@@ -24,12 +25,71 @@ class list {
   list()
   {
     head.next = &head;
+    head.prev = &head;
+  }
+  node_pointer get_node() {return new node_type;}
+  void insert(node_pointer pos, node_pointer p, const T& value)
+  {
+    p->prev = pos->prev;
+    pos->prev = p;
+    p->next = pos;
+    p->prev->next = p;
   }
   void push_front(const T& v)
   {
-    node_pointer p = new node_type(head.next, v);
-    head.next = p;
+    auto p = get_node();
+    p->info = v;
+    insert(head.next, p, v);
   }
+  void push_back(const T& v)
+  {
+    auto p = get_node();
+    p->info = v;
+    insert(&head, p, v);
+  }
+  void print() const
+  {
+    node_pointer p = head.next;
+    while (p != &head) {
+      std::cout << p->info << std::endl;
+      p = p->next;
+    }
+  }
+  void reverse()
+  {
+    node_pointer p = &head;
+    do {
+      std::swap(p->next, p->prev);
+      p = p->prev;
+    } while (p != &head);
+  }
+  //void swap_nodes(node_pointer p, node_pointer q)
+  //{
+  //  p->next->prev = q;
+  //  q->next->prev = p;
+
+  //  p->prev->next = q;
+  //  q->prev->next = p;
+
+  //  std::swap(p->prev, q->prev);
+  //  std::swap(p->next, q->next);
+  //}
+  //void sort()
+  //{
+  //  node_pointer p = head.next->next;
+  //  while (p != &head) {
+  //    node_pointer q = head.next;
+  //    while (q != p) {
+  //      if (p->info < q->info) {
+  //        swap_nodes(p, q);
+  //        p = q->next;
+  //        break;
+  //      }
+  //      q = q->next;
+  //    }
+  //    p = p->next;
+  //  }
+  //}
   node_pointer begin() {return head.next;}
   node_pointer end() {return &head;}
 };
--- src/tests/rt_list.cpp
+++ src/tests/rt_list.cpp
@@ -11,16 +11,22 @@ int main()
   using namespace rt;
 
   list<int> l;
-  l.push_front(1);
-  l.push_front(2);
-  l.push_front(3);
   l.push_front(4);
-  auto p = l.begin();
-  auto end = l.end();
-  while (p != end) {
-    std::cout << p->info << std::endl;
-    p = p->next;
-  }
+  l.push_front(3);
+  l.push_front(2);
+  l.push_front(1);
+  l.push_back(5);
+  l.push_back(6);
+  l.push_back(7);
+  l.push_back(8);
+  l.print();
+  std::cout << "________" << std::endl;
+  l.reverse();
+  l.print();
+  std::cout << "________" << std::endl;
+  //l.sort();
+  l.print();
+  std::cout << "________" << std::endl;
 
   return 0;
 }
