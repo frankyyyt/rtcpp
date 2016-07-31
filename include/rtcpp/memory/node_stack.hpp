0c03770fb4fc6f05c3c712a5a625178ad1501c67
Marcelo <mzimbres@gmail.com>
2016-07-31 14:21:33 -0300

    link_stack function moved to node_stack header.

--- include/rtcpp/memory/node_stack.hpp
+++ include/rtcpp/memory/node_stack.hpp
@@ -8,11 +8,34 @@
 #include <memory>
 #include <algorithm>
 
-#include "link_stack.hpp"
 #include "align.hpp"
 #include "node_alloc_header.hpp"
 
 namespace rt {
+
+// Expects the pointer p to have a suitable alignment for both T and
+// L.
+
+template <class T, class L>
+void link_stack(L* p, std::size_t n)
+{
+  constexpr auto N = sizeof (L);
+  constexpr auto S = sizeof (T);
+  constexpr auto R = S / N;
+  constexpr auto K = sizeof (std::size_t);
+
+  if (N < K) {
+    const std::size_t max = R * std::numeric_limits<L>::max();
+    if (n > max)
+      n = max;
+  }
+
+  const auto m = n / R; // Number of blocks of size S available.
+
+  p[0] = m - 1;
+  for (std::size_t i = 1; i < m; ++i)
+    p[i * R] = i - 1;
+}
 
 template <class T, class L>
 class node_stack {
