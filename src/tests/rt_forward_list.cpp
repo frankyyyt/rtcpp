1c6112de1ab1c5c4a6e61160533adebe18aa99ec
Marcelo <mzimbres@gmail.com>
2016-12-17 15:37:18 -0200

    reorganiztion of linked forward list tests.

--- src/tests/rt_forward_list.cpp
+++ src/tests/rt_forward_list.cpp
@@ -72,7 +72,7 @@ int foo::n_mv_ctor = 0;
 int foo::n_copy_assignment = 0;
 int foo::n_mv_assignment = 0;
 
-bool test_push_front_copy()
+void test_push_front_copy()
 {
   std::cout << "test_push_front_copy" << std::endl;
   foo::reset();
@@ -85,21 +85,18 @@ bool test_push_front_copy()
   l.push_front(bar2);
 
   if (foo::n_default_ctor != 3)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_copy_ctor != 2)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_mv_ctor != 1)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_copy_assignment != 0)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_mv_assignment != 0)
-    return false;
-  //if (foo::n_dctor != 5)
-  //  return false;
-  return true;
+    throw std::runtime_error("forward_list");
 }
 
-bool test_push_front_move()
+void test_push_front_move()
 {
   std::cout << "test_push_front_move" << std::endl;
   foo::reset();
@@ -112,91 +109,92 @@ bool test_push_front_move()
   const foo bar2;
   l.push_front(bar2);
   if (foo::n_default_ctor != 5)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_copy_ctor != 2)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_mv_ctor != 3)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_copy_assignment != 0)
-    return false;
+    throw std::runtime_error("forward_list");
   if (foo::n_mv_assignment != 0)
-    return false;
-  //if (foo::n_dctor != 5)
-  //  return false;
-  return true;
+    throw std::runtime_error("forward_list");
+}
+
+void test_basic()
+{
+    const int size = 10;
+
+    const int a = 1;
+    const int b = size;
+
+    const auto data = make_rand_data<int>(size, a, b, 1);
+    forward_list<int> l;
+
+    std::copy(std::begin(data), std::end(data), std::front_inserter(l));
+
+    const bool b1 = std::equal(std::rbegin(data), std::rend(data),
+                               std::begin(l));
+    if (!b1)
+      throw std::runtime_error("forward_list");
+
+    l.reverse();
+    const bool b2 = std::equal(std::begin(data), std::end(data),
+                               std::begin(l));
+    if (!b2)
+      throw std::runtime_error("forward_list");
+
+    l.reverse();
+    const bool b3 = std::equal(std::rbegin(data), std::rend(data),
+                               std::begin(l));
+    if (!b3)
+      throw std::runtime_error("forward_list");
+
+    l.insertion_sort();
+
+    if (!std::is_sorted(std::begin(l), std::end(l)))
+      throw std::runtime_error("forward_list");
+
+    print(l);
+    l.sorted_insertion(3);
+    l.sorted_insertion(-1);
+    l.sorted_insertion(20);
+    l.sorted_insertion(7);
+    print(l);
+
+    if (!std::is_sorted(std::begin(l), std::end(l)))
+      throw std::runtime_error("forward_list");
+
+    l.push_front(10101);
+
+    if (std::is_sorted(std::begin(l), std::end(l)))
+      throw std::runtime_error("forward_list");
+
+    l.remove_if(1);
+    l.remove_if(2);
+    l.remove_if(4);
+
+    l.clear();
+    if (!l.empty())
+      throw std::runtime_error("forward_list");
+
+    l.clear();
+    l.insert_after(l.begin(), 3, 10);
+    l.push_front(7);
+    l.insert_after(l.begin(), 3, 20);
+    l.push_front(7);
+    print(l);
 }
 
 int main()
 {
-
-  const int size = 10;
-
-  const int a = 1;
-  const int b = size;
-
-  const auto data = make_rand_data<int>(size, a, b, 1);
-  forward_list<int> l;
-
-  std::copy(std::begin(data), std::end(data), std::front_inserter(l));
-
-  const bool b1 = std::equal(std::rbegin(data), std::rend(data),
-                             std::begin(l));
-  if (!b1)
-    return 1;
-
-  l.reverse();
-  const bool b2 = std::equal(std::begin(data), std::end(data),
-                             std::begin(l));
-  if (!b2)
-    return 1;
-
-  l.reverse();
-  const bool b3 = std::equal(std::rbegin(data), std::rend(data),
-                             std::begin(l));
-  if (!b3)
-    return 1;
-
-  l.insertion_sort();
-
-  if (!std::is_sorted(std::begin(l), std::end(l))) {
+  try {
+    test_basic();
+    test_push_front_copy();
+    test_push_front_move();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
     return 1;
   }
-
-  print(l);
-  l.sorted_insertion(3);
-  l.sorted_insertion(-1);
-  l.sorted_insertion(20);
-  l.sorted_insertion(7);
-  print(l);
-
-  if (!std::is_sorted(std::begin(l), std::end(l)))
-    return 1;
-
-  l.push_front(10101);
-
-  if (std::is_sorted(std::begin(l), std::end(l)))
-    return 1;
-
-  l.remove_if(1);
-  l.remove_if(2);
-  l.remove_if(4);
-
-  l.clear();
-  if (!l.empty())
-    return 1;
-
-  if (!test_push_front_copy())
-    return 1;
-
-  if (!test_push_front_move())
-    return 1;
-  
-  l.clear();
-  l.insert_after(l.begin(), 3, 10);
-  l.push_front(7);
-  l.insert_after(l.begin(), 3, 20);
-  l.push_front(7);
-  print(l);
 
   return 0;
 }
